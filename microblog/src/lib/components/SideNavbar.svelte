699516c1d85ea6d9267c9b5038b96df53aab5c28
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-15 22:27:38 +0700

    side navbar

---
+++ microblog/src/lib/components/SideNavbar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import HomeIcon from '~icons/material-symbols/home';
+	import SearchIcon from '~icons/material-symbols/search';
+	import ChatIcon from '~icons/material-symbols/chat-bubble';
+	import NotificationsIcon from '~icons/material-symbols/notifications';
+	import ProfileIcon from '~icons/material-symbols/person';
+</script>
+
+<div class="h-full flex flex-col justify-between">
+	<ul class="menu gap-4">
+		<li>
+			<button type="button" class="active">
+				<HomeIcon class="text-xl" />
+				<span>Home</span>
+			</button>
+		</li>
+		<li>
+			<button type="button">
+				<SearchIcon class="text-xl" />
+				<span>Search</span>
+			</button>
+		</li>
+		<li>
+			<button type="button">
+				<ChatIcon class="text-xl" />
+				<span>Chat</span>
+			</button>
+		</li>
+		<li>
+			<button type="button">
+				<NotificationsIcon class="text-xl" />
+				<span>Notifications</span>
+			</button>
+		</li>
+		<li>
+			<button type="button">
+				<ProfileIcon class="text-xl" />
+				<span>Profile</span>
+			</button>
+		</li>
+	</ul>
+</div>
