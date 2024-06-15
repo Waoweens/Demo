cfeeb46cee41d0968a5a920ceee8632739a9c447
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-15 22:27:12 +0700

    bottom navigation bar

---
+++ microblog/src/lib/components/BottomNavbar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import HomeIcon from '~icons/material-symbols/home';
+	import SearchIcon from '~icons/material-symbols/search';
+	import ChatIcon from '~icons/material-symbols/chat-bubble';
+	import NotificationsIcon from '~icons/material-symbols/notifications';
+	import ProfileIcon from '~icons/material-symbols/person';
+</script>
+
+<div class="btm-nav">
+	<button type="button" class="active">
+		<HomeIcon class="text-xl" />
+	</button>
+	<button type="button">
+		<SearchIcon class="text-xl" />
+	</button>
+	<button type="button">
+		<ChatIcon class="text-xl" />
+	</button>
+	<button type="button">
+		<NotificationsIcon class="text-xl" />
+	</button>
+	<button type="button">
+		<ProfileIcon class="text-xl" />
+	</button>
+</div>
