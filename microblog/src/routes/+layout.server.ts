6b03bedb5f9393b454637da3811a1b3459b2c758
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-16 20:34:51 +0700

    also send user id

--- microblog/src/routes/+layout.server.ts
+++ microblog/src/routes/+layout.server.ts
@@ -5,6 +5,7 @@ export const load: LayoutServerLoad = async (event) => {
 
 	return {
 		signedIn: true,
-		username: event.locals.user.username
+		username: event.locals.user.username,
+		userId: event.locals.user.id
 	};
 };
