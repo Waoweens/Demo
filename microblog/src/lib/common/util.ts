d27731cc9925e39c46f5c193bbf3792d7353865c
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-16 20:28:13 +0700

    TimelinePost type

--- microblog/src/lib/common/util.ts
+++ microblog/src/lib/common/util.ts
@@ -1,3 +1,23 @@
 import SnowflakeGenerator from "./snowflake";
 
 export const snowflake = new SnowflakeGenerator(1);
+console.log(snowflake.generate());
+
+export type TimelinePost = {
+	id: bigint,
+	content: string | null,
+	createdAt: Date,
+	yeahed?: boolean,
+	author: {
+		id: bigint,
+		username: string,
+		displayName: string | null,
+		profileImage?: string | null
+	}
+	_count: {
+		yeahs: number,
+		reposts: number,
+		replies: number
+		quotes: number
+	}
+}
