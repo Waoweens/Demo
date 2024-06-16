1a532606f68a07c973477b672d9b1eb9acc21a22
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-16 22:08:07 +0700

    move core page layout to layout

--- microblog/src/routes/+layout.svelte
+++ microblog/src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import BottomNavbar from '$components/BottomNavbar.svelte';
 	import Navbar from '$components/Navbar.svelte';
+	import SideNavbar from '$components/SideNavbar.svelte';
 	import '../app.css';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
 </script>
 
 <div class="flex flex-col h-screen">
@@ -10,8 +14,16 @@
 			<Navbar />
 		</nav>
 	</header>
-	<div class="flex-1">
-		<slot />
+	<div class="flex-1 flex h-full justify-center">
+		<div class="flex w-full max-w-6xl">
+			<aside class="hidden md:block basis-1/5">
+				<SideNavbar authStatus={data.signedIn} />
+			</aside>
+			<main class="flex-1 border-x border-neutral">
+				<slot />
+			</main>
+			<aside class="hidden md:block basis-1/5">Right sidebar</aside>
+		</div>
 	</div>
 	<footer>
 		<nav class="md:hidden">
--- microblog/src/routes/+page.svelte
+++ microblog/src/routes/+page.svelte
@@ -7,34 +7,22 @@
 
 	export let data: PageData & LayoutData;
 	const authStatus = data.signedIn;
-
-	// $: console.log(data);
 </script>
 
-<div class="flex h-full justify-center">
-	<div class="flex w-full flex-row max-w-6xl">
-		<aside class="hidden md:block basis-1/5">
-			<SideNavbar authStatus={data.signedIn} />
-		</aside>
-		<main class="flex-1 border-x border-neutral flex flex-col">
-			<div role="tablist" class="tabs tabs-lg tabs-bordered [--tab-border:0.2rem]">
-				<button type="button" role="tab" class="tab tab-active tab-custom-color"
-					>Chronological</button
-				>
-				<button type="button" role="tab" class="tab">Following</button>
-			</div>
-			<div class="border-b border-neutral">
-				{#if data.signedIn}
-					<Composer />
-				{:else}
-					<div class="m-4 flex justify-between items-center">
-						<p class="text-2xl">Sign in to post</p>
-						<a href="/auth" class="btn btn-sm btn-primary">Sign in</a>
-					</div>
-				{/if}
-			</div>
-			<Timeline {authStatus} posts={data.posts}  />
-		</main>
-		<aside class="hidden md:block basis-1/5">Right sidebar</aside>
+<div class="flex flex-col">
+	<div role="tablist" class="tabs tabs-lg tabs-bordered [--tab-border:0.2rem]">
+		<button type="button" role="tab" class="tab tab-active tab-custom-color">Chronological</button>
+		<button type="button" role="tab" class="tab">Following</button>
 	</div>
+	<div class="border-b border-neutral">
+		{#if authStatus}
+			<Composer />
+		{:else}
+			<div class="m-4 flex justify-between items-center">
+				<p class="text-2xl">Sign in to post</p>
+				<a href="/auth" class="btn btn-sm btn-primary">Sign in</a>
+			</div>
+		{/if}
+	</div>
+	<Timeline {authStatus} posts={data.posts} />
 </div>
