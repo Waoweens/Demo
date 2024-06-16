7746e6b1ed8e32d9d0557161af27987a65682bba
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-16 14:20:33 +0700

    use auth status

--- microblog/src/routes/+page.svelte
+++ microblog/src/routes/+page.svelte
@@ -1,22 +1,33 @@
 <script lang="ts">
-	import Composer from "$lib/components/Composer.svelte";
-	import SideNavbar from "$lib/components/SideNavbar.svelte";
-	import Timeline from "$lib/components/Timeline.svelte";
+	import Composer from '$components/Composer.svelte';
+	import SideNavbar from '$components/SideNavbar.svelte';
+	import Timeline from '$components/Timeline.svelte';
+	import type { LayoutData } from './$types';
 
+	export let data: LayoutData;
 </script>
 
 <div class="flex h-full justify-center">
 	<div class="flex w-full flex-row max-w-6xl">
 		<aside class="hidden md:block basis-1/5">
-			<SideNavbar />
+			<SideNavbar authStatus={data.signedIn} />
 		</aside>
-		<main class="flex-1 border border-neutral flex flex-col">
+		<main class="flex-1 border-x border-neutral flex flex-col">
 			<div role="tablist" class="tabs tabs-lg tabs-bordered [--tab-border:0.2rem]">
-				<button type="button" role="tab" class="tab tab-active tab-custom-color">Chronological</button>
+				<button type="button" role="tab" class="tab tab-active tab-custom-color"
+					>Chronological</button
+				>
 				<button type="button" role="tab" class="tab">Following</button>
 			</div>
 			<div class="border-b border-neutral">
-				<Composer />
+				{#if data.signedIn}
+					<Composer />
+				{:else}
+					<div class="m-4 flex justify-between items-center">
+						<p class="text-2xl">Sign in to post</p>
+						<a href="/auth" class="btn btn-sm btn-primary">Sign in</a>
+					</div>
+				{/if}
 			</div>
 			<Timeline />
 		</main>
