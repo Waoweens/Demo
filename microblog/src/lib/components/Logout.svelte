0316b2c4fb70be13dc258d6ea8fe656ba51e9ded
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-16 14:17:59 +0700

    logout helper component

---
+++ microblog/src/lib/components/Logout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { enhance } from "$app/forms";
+
+	export let style: string = '';
+</script>
+
+<form use:enhance action="/auth?/logout" method="post">
+	<button class={'flex items-center gap-2 ' +style} type="submit">
+		<slot />
+	</button>
+</form>
