4286497ea6b7bdb1b25aa71ccbbbdb54a3888c8d
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-16 22:28:41 +0700

    make sign in banner for mobile

--- microblog/src/lib/components/BottomNavbar.svelte
+++ microblog/src/lib/components/BottomNavbar.svelte
@@ -4,22 +4,33 @@
 	import ChatIcon from '~icons/material-symbols/chat-bubble';
 	import NotificationsIcon from '~icons/material-symbols/notifications';
 	import ProfileIcon from '~icons/material-symbols/person';
+
+	export let authStatus: boolean = false;
 </script>
 
-<div class="btm-nav">
-	<button type="button" class="active">
-		<HomeIcon class="text-xl" />
-	</button>
-	<button type="button" disabled>
-		<SearchIcon class="text-xl" />
-	</button>
-	<button type="button" disabled>
-		<ChatIcon class="text-xl" />
-	</button>
-	<button type="button">
-		<NotificationsIcon class="text-xl" />
-	</button>
-	<button type="button">
-		<ProfileIcon class="text-xl" />
-	</button>
-</div>
+{#if authStatus}
+	<div class="btm-nav">
+		<button type="button" class="active">
+			<HomeIcon class="text-xl" />
+		</button>
+		<button type="button" disabled>
+			<SearchIcon class="text-xl" />
+		</button>
+		<button type="button" disabled>
+			<ChatIcon class="text-xl" />
+		</button>
+		<button type="button">
+			<NotificationsIcon class="text-xl" />
+		</button>
+		<button type="button">
+			<ProfileIcon class="text-xl" />
+		</button>
+	</div>
+{:else}
+	<div class="btm-nav">
+		<div class="flex flex-row justify-between px-10 bg-neutral text-neutral-content">
+			<span class="text-xl">Not signed in</span>
+			<a href="/auth/login" class="btn btn-sm btn-primary">Sign in</a>
+		</div>
+	</div>
+{/if}
