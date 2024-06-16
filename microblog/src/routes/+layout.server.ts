9cd3622219c4334beacace00b1dd53efbef5700b
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-16 14:19:09 +0700

    auth checking

---
+++ microblog/src/routes/+layout.server.ts
@@ -0,0 +1,10 @@
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = async (event) => {
+	if (!event.locals.user) return { signedIn: false };
+
+	return {
+		signedIn: true,
+		username: event.locals.user.username
+	};
+};
