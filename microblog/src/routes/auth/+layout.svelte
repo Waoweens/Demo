4a4c07707a4790e0cdb6bdb4d15336d8016ba5f9
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-16 14:19:52 +0700

    change card width

--- microblog/src/routes/auth/+layout.svelte
+++ microblog/src/routes/auth/+layout.svelte
@@ -1,5 +1,5 @@
 <div class="h-full flex justify-center items-center">
-	<main class="card bg-base-200 shadow-xl p-10">
+	<main class="card w-96 bg-base-200 shadow-xl p-10">
 		<slot></slot>
 	</main>
 </div>
