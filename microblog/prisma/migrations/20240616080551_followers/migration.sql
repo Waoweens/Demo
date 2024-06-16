6f3a87391ea892b1197f6a32248f6c5adb5ff52a
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-16 15:05:58 +0700

    migrate

---
+++ microblog/prisma/migrations/20240616080551_followers/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Reply` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Reply" DROP CONSTRAINT "Reply_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Reply" DROP CONSTRAINT "Reply_postId_fkey";
+
+-- DropTable
+DROP TABLE "Reply";
+
+-- CreateTable
+CREATE TABLE "Follower" (
+    "id" SERIAL NOT NULL,
+    "followerId" BIGINT NOT NULL,
+    "followingId" BIGINT NOT NULL,
+
+    CONSTRAINT "Follower_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Follower_followerId_followingId_key" ON "Follower"("followerId", "followingId");
+
+-- AddForeignKey
+ALTER TABLE "Follower" ADD CONSTRAINT "Follower_followerId_fkey" FOREIGN KEY ("followerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follower" ADD CONSTRAINT "Follower_followingId_fkey" FOREIGN KEY ("followingId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
