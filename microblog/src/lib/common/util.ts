27f51f6796fc63047a018bde4f96a0aea25f4326
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-17 20:39:07 +0700

    format

--- microblog/src/lib/common/util.ts
+++ microblog/src/lib/common/util.ts
@@ -1,23 +1,29 @@
-import SnowflakeGenerator from "./snowflake";
+import SnowflakeGenerator from './snowflake';
 
 export const snowflake = new SnowflakeGenerator(1);
 console.log(snowflake.generate());
 
 export type TimelinePost = {
-	id: bigint,
-	content: string | null,
-	createdAt: Date,
-	yeahed?: boolean,
+	id: bigint;
+	content: string | null;
+	createdAt: Date;
+	yeahed?: boolean;
 	author: {
-		id: bigint,
-		username: string,
-		displayName: string | null,
-		profileImage?: string | null
-	}
+		id: bigint;
+		username: string;
+		displayName: string | null;
+		profileImage?: string | null;
+	};
 	_count: {
-		yeahs: number,
-		reposts: number,
-		replies: number
-		quotes: number
-	}
-}
+		yeahs: number;
+		reposts: number;
+		replies: number;
+		quotes: number;
+	};
+};
+
+export type ModalActions = {
+	id: string;
+	open: () => void;
+	close: () => void;
+};
