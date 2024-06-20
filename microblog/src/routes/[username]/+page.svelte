b86e877b77da5eae145b0643d57458ecbdb4ffcc
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-20 09:29:32 +0700

    fix title

--- microblog/src/routes/[username]/+page.svelte
+++ microblog/src/routes/[username]/+page.svelte
@@ -16,7 +16,7 @@
 </script>
 
 <div>
-	<BackButton />
+	<BackButton title={data.user.displayName ?? 'Unknown user'} />
 	<div class="flex flex-col border-b border-neutral">
 		<img class="aspect-[3/1] w-full" src={data.user.bannerImage} alt="user banner" />
 		<div class="avatar ml-8 bottom-16 -mb-16">
