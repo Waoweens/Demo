8efe97e5b25465eee7dcc1cb924be093245bcdd8
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-20 18:58:27 +0700

    fancy it up

--- microblog/src/routes/auth/redirect-root/+page.svelte
+++ microblog/src/routes/auth/redirect-root/+page.svelte
@@ -1,1 +1,8 @@
+<script>
+	import { pageMeta } from "$lib/stores/page";
+
+	pageMeta.set({ pageTitle: 'Redirecting' })
+</script>
+
 <h1>REDIRECTING</h1>
+<p>Not redirected? <a href="/" class="link">Click here.</a></p>
