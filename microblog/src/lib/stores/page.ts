29363afcc935dfbbfa93b90e3ca7589b9fbb8719
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-20 18:39:40 +0700

    allow null

--- microblog/src/lib/stores/page.ts
+++ microblog/src/lib/stores/page.ts
@@ -1,11 +1,11 @@
 import { writable } from "svelte/store";
 
 export const pageMeta = writable<{
-	pageTitle?: string;
-	ogTitle?: string;
-	description?: string;
-	url?: string;
-	date?: string;
+	pageTitle?: string | null;
+	ogTitle?: string | null;
+	description?: string | null;
+	url?: string | null;
+	date?: string | null;
 }>({})
 export const pageHistory = writable<string[]>(['/']);
 export const isGoingBack = writable<boolean>(false);
