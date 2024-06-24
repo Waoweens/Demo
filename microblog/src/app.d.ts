b821daf2b626d608b01487278c0f0af31f96e08a
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-24 21:47:00 +0700

    fix types

--- microblog/src/app.d.ts
+++ microblog/src/app.d.ts
@@ -1,17 +1,22 @@
 import 'unplugin-icons/types/svelte'
-import type { CropperProps } from 'svelte-cropper';
 
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
+
 		interface Locals {
 			user: import("lucia").User | null;
 			session: import("lucia").Session | null;
 		}
 		// interface PageData {}
-		// interface PageState {}
+
+		// at this point i don't care yet
+		interface PageState {
+			[key: string]: unknown;
+		}
+
 		// interface Platform {}
 	}
 }
